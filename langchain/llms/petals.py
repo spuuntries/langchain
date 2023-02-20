a8737905588d142c1c8e15ab150975ef1edddd9b
spuun <69007078+spuuntries@users.noreply.github.com>
2023-02-20 09:28:56 +0700

    Update petals.py

--- langchain/llms/petals.py
+++ langchain/llms/petals.py
@@ -97,7 +97,7 @@ class Petals(LLM, BaseModel):
 
             model_name = values["model_name"]
             values["tokenizer"] = BloomTokenizerFast.from_pretrained(model_name)
-            values["client"] = DistributedBloomForCausalLM.from_pretrained(model_name, request_timeout=300).to("cuda")
+            values["client"] = DistributedBloomForCausalLM.from_pretrained(model_name, request_timeout=300).cuda()
             values["huggingface_api_key"] = huggingface_api_key
 
         except ImportError:
@@ -133,7 +133,7 @@ class Petals(LLM, BaseModel):
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
         """Call the Petals API."""
         params = self._default_params
-        inputs = self.tokenizer(prompt, return_tensors="pt")["input_ids"]
+        inputs = self.tokenizer(prompt, return_tensors="pt")["input_ids"].cuda()
         outputs = self.client.generate(inputs, **params)
         text = self.tokenizer.decode(outputs[0])
         if stop is not None:
