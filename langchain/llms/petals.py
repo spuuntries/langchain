193b3d9711f7624a11c6b6e56e417ff55a8f43a5
spuun <69007078+spuuntries@users.noreply.github.com>
2023-02-20 08:26:42 +0700

    Update petals.py

--- langchain/llms/petals.py
+++ langchain/llms/petals.py
@@ -97,7 +97,7 @@ class Petals(LLM, BaseModel):
 
             model_name = values["model_name"]
             values["tokenizer"] = BloomTokenizerFast.from_pretrained(model_name)
-            values["client"] = DistributedBloomForCausalLM.from_pretrained(model_name)
+            values["client"] = DistributedBloomForCausalLM.from_pretrained(model_name, request_timeout=300)
             values["huggingface_api_key"] = huggingface_api_key
 
         except ImportError:
